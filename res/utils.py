26fec6e58d85d348bf271e35ebbe7ae8db24e1c8
leuhvraiminoryo <50586611+leuhvraiminoryo@users.noreply.github.com>
2022-05-28 11:07:11 +0200

    remove commented useless code

--- res/utils.py
+++ res/utils.py
@@ -1,19 +1,5 @@
 from res.const import *
 
-
-'''def verifKeyPresses(pressed):
-    for event in pygame.event.get():
-        if event.type == MOUSEBUTTONDOWN:
-            if event.button == 1:
-                pressed['mleft'] = True
-            if event.button == 3:
-                pressed['mright'] = True
-        if event.type == MOUSEBUTTONUP:
-            if event.button == 1:
-                pressed['mleft'] = False
-            if event.button == 3:
-                pressed['mright'] = False'''
-
 def terminate():
     pygame.image.save(CANVAS,'pic.png')
     pygame.quit()
